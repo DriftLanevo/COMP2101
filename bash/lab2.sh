4c989e037a21c5320db276910926b585d6d99f3d
DriftLanevo <200347348@student.georgianc.on.ca>
2018-03-05 13:54:04 -0500

    last

--- bash/lab2.sh
+++ bash/lab2.sh
@@ -25,3 +25,56 @@ Today is $weekday.
 "
 #Use cowsay program to display the content of welcome_meesage
 echo $welcome_message | cowsay -f dragon
+
+
+################################################################
+host=`hostname`
+domain=`hostname -d`
+if [ -n $host ]
+then
+        echo "The system name is $(hostname)"
+else
+        echo "There is no hostname"
+fi 
+
+if [ -z $domain ]
+then
+        echo "There is no domain name"
+else
+        echo "The domain name is $domain"
+fi
+
+a=`ip r show 2>STERR | grep "src" | sed q | cut -d " " -f 2`
+if [ -z $a ] 
+then
+        echo "The network is not configured"
+else
+        echo "$(ip r show | grep "src" | cut -d " " -f 3,12)"
+fi
+
+name=`cat /etc/os-release | head -n 2 | grep "NAME" | cut -d "=" -f 2 | cut -d '"' -f 2`
+echo "The system name is $name"
+
+os=`cat /etc/os-release | head -n 2 | grep "VERSION" | cut -d "=" -f 2 | cut -d '"' -f 2 | cut -d " " -f 1`
+echo "The OS version is $os"
+
+echo "The CPU description is following:"
+cpu=`lscpu | grep "Model name" | cut -d ":" -f 2 ` 
+echo ${cpu}
+
+echo "The memory installed information is listed below:"
+mem=`free -h | grep "Mem" | awk '{print $2}'` 
+echo "The total memory is $mem"
+
+echo "The available disk space on attached physical drives information include the following:"
+echo "  Disk(s)       Available"
+echo `df -h /dev/sd* | grep /dev/ | awk '{print $1}'` " " " " " " `df -h /dev/sd* | grep /dev/ | awk '{print $4}'`
+
+b=`lpstat -p 2>STERR | grep "printer" | cut -d " " -f 2`
+if [ -z $b ]
+then
+        echo "There is no configured printer"
+else
+        echo "The configured printer(s) is( are):" ; echo `lpstat -p | grep "printer" | cut -d " " -f 2`
+fi
+
---
+++ bash/zhua.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "who is Zhang Ze"
+
+read name
+
+echo "You are right! Zhange Ze is $name" 
